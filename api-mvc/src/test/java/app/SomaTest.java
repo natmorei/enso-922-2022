6d9a1b5229545feb12a99fa91d399ca1c24a36be
Renata Moreira dos Santos <rms16@aluno.ifal.edu.br>
2022-10-03 14:22:15 +0000

    TestSoma

--- api-mvc/src/test/java/app/SomaTest.java
+++ api-mvc/src/test/java/app/SomaTest.java
@@ -14,18 +14,18 @@ public class SomaTest {
     @Test
     public void testBase(){
         double result = soma.Soma(12, 4);
-        assertEquals(8,result);
+        assertEquals(16,result);
     }
 
 
     @ Test
-    public void subDedecimais(){
+    public void somaDedecimais(){
         double result = soma.Soma(10, 3.5);
-        assertEquals(5.5,result);
+        assertEquals(13.5,result);
     }
 
     @Test
-    public void  subComZero(){
+    public void  somaComZero(){
         double resut = soma.Soma(10, 0);
         assertEquals(10,resut);
     }
